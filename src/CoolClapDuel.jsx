1471ff8a906d459ab5b154d228c1f9d6248f479d
A-luo0628 <qiheluo@gmail.com>
2025-05-12 18:52:23 +0800

    Initial commit
    Initial commit

---
+++ src/CoolClapDuel.jsx
@@ -0,0 +1,4 @@
+
+export default function CoolClapDuel() {
+  return <div style={{ textAlign: 'center', padding: '4rem' }}>Cool Clap Duel 游戏组件加载成功！</div>;
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CoolClapDuel from './CoolClapDuel'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CoolClapDuel />
+  </React.StrictMode>
+)
